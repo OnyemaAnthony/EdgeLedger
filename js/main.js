fed65c6bcabf5f19f63f8d969a1430adb41e9b54
OnyemaAnthony <Anthonychibuike246@gmail.com>
2021-07-28 23:26:44 +0000

    finish footer

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// let map;
+//
+// function initMap() {
+//     map = new google.maps.Map(document.getElementById("map"), {
+//         center: { lat: -34.397, lng: 150.644 },
+//         zoom: 8,
+//     });
+// }
+
+function initMap(){
+    const  loc = {lat:42.361145,lng:-71.057083};
+    const  map = new google.maps.Map(document.querySelector('.map'),{
+        zoom: 14,
+        center: loc
+    });
+    const  marker = new google.maps.Marker({position:loc,map:map});
+}
