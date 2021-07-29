398072758a0ca4dd3933cb3f38d6df19acfe57bb
OnyemaAnthony <Anthonychibuike246@gmail.com>
2021-07-29 13:43:53 +0000

    finish mobile responsive

--- js/main.js
+++ js/main.js
@@ -7,29 +7,30 @@
 //     });
 // }
 
-function initMap(){
-    const  loc = {lat:42.361145,lng:-71.057083};
-    const  map = new google.maps.Map(document.querySelector('.map'),{
+function initMap() {
+    const loc = {lat: 42.361145, lng: -71.057083};
+    const map = new google.maps.Map(document.querySelector('.map'), {
         zoom: 14,
         center: loc
     });
-    const  marker = new google.maps.Marker({position:loc,map:map});
+    const marker = new google.maps.Marker({position: loc, map: map});
 }
-window.addEventListener('scroll',function (){
-    if(window.scrollY > 150){
-        document.querySelector('.nav').style.opacity =0.9;
-    }else{
+
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 150) {
+        document.querySelector('.nav').style.opacity = 0.9;
+    } else {
         document.querySelector('.nav').style.opacity = 1;
     }
 })
 
-$('.nav a, .btn').on('click',function (event){
-    if(this.hash !=''){
+$('.nav a, .btn').on('click', function (event) {
+    if (this.hash != '') {
         event.preventDefault();
-        const  hash = this.hash;
+        const hash = this.hash;
         $('html, body').animate(
             {
-                scrollTop:$(hash).offset().top-100
+                scrollTop: $(hash).offset().top - 100
             },
             800
         );
